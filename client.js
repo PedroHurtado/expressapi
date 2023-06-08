dfe5513400c6267549e4b33496fd1a2263862ea1
Pedro Hurtado <pedrohurt@gmail.com>
2023-06-08 13:45:26 +0000

    finish

--- client.js
+++ client.js
@@ -1,4 +1,19 @@
+import { json } from "express";
+
 const baseUrl = 'http://localhost:8080'
+
+export async function getPizzas() {   
+    const response = await fetch(`${baseUrl}/pizzas`, {
+        method: 'GET',        
+    })
+    const data = await response.json();
+    const {status} = response;
+    console.log({
+        method:'GET',
+        status,
+        data
+    })
+}
 
 export async function postPizza() {
     const pizza = {
@@ -6,6 +21,7 @@ export async function postPizza() {
     }
     const response = await fetch(`${baseUrl}/pizzas`, {
         method: 'POST',
+        
         headers: {
             'content-type': 'application/json'
         },
@@ -14,7 +30,49 @@ export async function postPizza() {
     const data = await response.json();
     const {status} = response;
     console.log({
+        method:'POST',
         status,
         data
+    })
+}
+
+
+export async function getPizza(id) {   
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'GET',        
+    })
+    const data = await response.json();
+    const {status} = response;
+    console.log({
+        method:'GET',
+        status,
+        data
+    })
+}
+
+export async function deletePizza(id) {   
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'DELETE',        
+    })   
+    const {status} = response;
+    console.log({
+        method:'DELETE',
+        status,        
+    })
+}
+
+export async function upatePizza(pizza) {   
+    const id = pizza
+    const response = await fetch(`${baseUrl}/pizzas/${id}`, {
+        method: 'PUT',        
+        headers:{
+            'content-type':'application/json'
+        },
+        body:JSON.stringify(pizza)
+    })   
+    const {status} = response;
+    console.log({
+        method:'PUT',
+        status,        
     })
 }
--- serve.js
+++ serve.js
@@ -1,5 +1,5 @@
 import expres from "express";
-import {postPizza} from './client.js'
+import {deletePizza, getPizza, getPizzas, postPizza, upatePizza} from './client.js'
 
 const app = expres();
 
@@ -9,6 +9,40 @@ app.post('/pizzas',(req,res)=>{
     res.status(201).json(req.body)
 })
 
+app.get('/pizzas/:id',(req,res)=>{
+    res.status(200).json({
+        id:req.params.id,
+        name:'carbonara'
+    })
+})
+
+app.get('/pizzas',(req,res)=>{
+    res.status(200).json([{
+        id:req.params.id,
+        name:'carbonara'
+    }])
+})
+
+
+
+app.put('/pizzas/:id',(req,res)=>{    
+    console.log('put server', req.body)
+    res.status(204).end("")
+})
+
+app.delete('/pizzas/:id',(req,res)=>{
+    
+    console.log('delete server', req.params.id)
+    res.status(204).end("")
+})
+
 const server = app.listen(8080);
 
 postPizza();
+getPizzas();
+getPizza(1);
+deletePizza(1);
+upatePizza({
+    id:1,
+    name:'carbonara'
+})
